8042a2aab48722654a26b3ea6933a88ee7766bae
Douglas Cananéa <cananeadouglas@gmail.com>
2018-01-23 14:24:30 -0300

    oi

--- src/main/java/br/com/prointjsf/CadastroBean.java
+++ src/main/java/br/com/prointjsf/CadastroBean.java
@@ -7,37 +7,19 @@ import javax.swing.JOptionPane;
 import br.com.dao.DaoGenerico;
 import br.com.proint.Cadastro;
 
-@ViewScoped
+//@ViewScoped
 @ManagedBean(name="cadastroBean")
 
 public class CadastroBean {
 	
 	private Cadastro cadastro = new Cadastro(); // objeto usado para o cadastro
 	private DaoGenerico<Cadastro> daoGenerico = new DaoGenerico<Cadastro>();
-	
-	private String title;
-	
-	
 
 	public String salvar() {
 		daoGenerico.salvar(cadastro);
 		cadastro = new Cadastro();
-		mostrar();
+		
 		return "";
-	}
-
-	
-	public void mostrar() {
-		setTitle("Usuário Salvo");
-	}
-	
-	
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
 	}
 	
 	public Cadastro getCadastro() {
